892f6f1666fe927a7f34e348a1bc31e58a443144
Hyunsu Joo <hs9880@gmail.com>
2020-11-27 19:00:46 +0900

    [Add] initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return<h1>
+    This is App just added eslint his is App just added eslint his is App just
+    added eslint adrtwertwertwetwetwetwertwe
+  </h1>
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+
+const app = (
+  <App />
+);
+render(app, document.getElementById('app'));
